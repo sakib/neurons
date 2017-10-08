dd5f33a01a971b6825a575e26b4d7a7bf7a38818
Sakib Jalal <sakib.jalal@gmail.com>
2017-10-08 10:45:30 -0400

    working on hh

--- neurons.py
+++ neurons.py
@@ -1,9 +1,10 @@
+""" Neuron implementation """
 import abc
-import operator
 import numpy as np
-import matplotlib.pyplot as plt
 
-DEFAULT_WAVELENGTH = 100
+TIME_STEPS = 250
+SPIKE_MULTIPLIER = 2.5
+IZHIKEVICH_SPIKE = 30 # 30 mV
 
 class Wave:
     """ Moving set of values """
@@ -11,18 +12,22 @@ class Wave:
         self.values = []
         assert cardinality > 0
         self.cardinality = cardinality
-        self.time_axis = [i for i in range(len(DEFAULT_WAVELENGTH))]
+        self.time_axis = [i for i in range(TIME_STEPS)]
 
     def append(self, new_value):
-        if len(self.values) == cardinality:
+        """ Add value to end of the series """
+        if len(self.values) == self.cardinality:
             self.values.pop(0)
         self.values.append(new_value)
 
-    def display(self):
-        x_values = np.arange(self.time_axis)
-        y_values = np.arange(self.values)
-        plt.plot(x_values, y_values)
-        plt.show()
+    def add_plot(self, plt, color=None):
+        """ Display value series as curve """
+        x_values = np.array(self.time_axis)
+        y_values = np.array(self.values)
+        if color is None:
+            plt.plot(x_values, y_values)
+        else:
+            plt.plot(x_values, y_values, color)
 
 class Neuron(object):
     """ Abstract base class for Neurons """
@@ -30,29 +35,96 @@ class Neuron(object):
 
     @abc.abstractmethod
     def __init__(self):
-        self.wave = Wave(DEFAULT_WAVELENGTH)
-        pass
+        self.wave = Wave(TIME_STEPS)
 
     @abc.abstractmethod
     def time_step(self, current):
+        """ Update voltage given a current at a time """
+        pass
+
+    @abc.abstractmethod
+    def add_plot(self, plt, color=None):
+        """ Adds neuron spike train to given pyplot """
         pass
 
 class LIF(Neuron):
     """ Implementation of leaky integrate-and-fire neuron model """
     def __init__(self, capacitance, resistance, spike_voltage, rest_voltage):
-        self.voltage = 0
+        Neuron.__init__(self)
+        self.voltage = rest_voltage
         self.res = resistance
         self.cap = capacitance
         self.v_max = spike_voltage
         self.v_rest = rest_voltage
+        self.wave.append(rest_voltage)
 
     def time_step(self, current):
-        self.voltage += current/self.cap - self.voltage/(self.cap*self.res)
+        self.voltage += current/self.cap
+        self.voltage -= self.voltage/(self.cap*self.res)
         if self.voltage > self.v_max:
             self.voltage = self.v_rest
-            self.wave.append(self.v_max*2)
+            self.wave.append(self.v_max*SPIKE_MULTIPLIER)
             self.wave.append(0)
         elif self.voltage < self.v_rest:
             self.voltage = self.v_rest
         self.wave.append(self.voltage)
 
+    def add_plot(self, plt, color=None):
+        self.wave.add_plot(plt, color)
+
+class Izhikevich(Neuron):
+    """ Implementation of Izhikevich neuron model """
+    def __init__(self, p_u, params):
+        Neuron.__init__(self)
+        p_a, p_b, p_c, p_d = params
+        self.voltage = p_c
+        self.p_u = p_u
+        self.p_a = p_a
+        self.p_b = p_b
+        self.p_c = p_c
+        self.p_d = p_d
+        self.wave.append(p_c)
+
+    def time_step(self, current):
+        self.voltage += (0.04*(self.voltage**2) + 5*self.voltage + 140 - self.p_u + current)
+        self.p_u += (self.p_a * (self.p_b*self.voltage - self.p_u))
+        if self.voltage >= IZHIKEVICH_SPIKE:
+            self.voltage = self.p_c
+            self.p_u += self.p_d
+            self.wave.append(IZHIKEVICH_SPIKE*SPIKE_MULTIPLIER)
+        elif self.voltage < self.p_c:
+            self.voltage = self.p_c
+        self.wave.append(self.voltage)
+
+    def add_plot(self, plt, color=None):
+        self.wave.add_plot(plt, color)
+
+class HodgkinHuxley(Neuron):
+    """ Implementation of Hodgkin-Huxley neuron model """
+    def __init__(self, hh_capacitance, hh_opts, hh_g, hh_v):
+        Neuron.__init__(self)
+        self.voltage = 0 #?
+        self.cap = hh_capacitance
+        self.opts = hh_opts
+        self.p_g = hh_g
+        self.p_v = hh_v
+        self.wave.append(0) #?
+
+    def time_step(self, current):
+        v_diffs = {}
+        v_diffs['Na'] = self.p_g['Na']*\
+                (self.opts['m']**3)*self.opts['h']*(self.voltage-self.p_v['Na'])
+        v_diffs['K'] = self.p_g['K']*\
+                (self.opts['n']**4)*(self.voltage-self.p_v['K'])
+        v_diffs['L'] = self.p_g['L']*\
+                (self.voltage-self.p_v['L'])
+        v_diff = (current - v_diffs['Na'] - v_diffs['K'] - v_diffs['L'])/self.cap
+        self.voltage += v_diff
+        if self.voltage >= HH_SPIKE:
+            # spike
+        elif self.voltage < self.minimum: #?
+            # reset
+        self.wave.append(self.voltage)
+
+    def add_plot(self, plt, color=None):
+        self.wave.add_plot(plt, color)
--- simulator.py
+++ simulator.py
@@ -1,18 +1,113 @@
-# neuron simulator
-from neurons import LIF
+""" Neuron Simulator """
+#from neurons import LIF, Izhikevich, HodgkinHuxley
+from neurons import HodgkinHuxley
+from neurons import IZHIKEVICH_SPIKE, SPIKE_MULTIPLIER, TIME_STEPS
+import matplotlib.pyplot as plt
+#from matplotlib.ticker import NullFormatter
 
-currents = []
+CURRENTS = []
+RESISTANCE = 2
+CAPACITANCE = 2.5
+REST_VOLTAGE = 10
+SPIKE_VOLTAGE = 75
 
-def constant_current(t):
-    return 50
+"""
+# Problem 1
+plt.figure(1)
+for x in range(10, 90, 20):
+    CURRENTS.append(x)
+for i in range(1, len(CURRENTS)+1):
+    neuron = LIF(CAPACITANCE, RESISTANCE, SPIKE_VOLTAGE, REST_VOLTAGE)
+    current = CURRENTS[i-1]
+    plt.subplot(810+i)
+    plt.title('Current: {}'.format(current))
+    for i in range(TIME_STEPS-1):
+        neuron.time_step(current)
+    neuron.add_plot(plt)
+    x1, x2, y1, y2 = plt.axis()
+    plt.axis((x1, x2, 0, SPIKE_VOLTAGE*SPIKE_MULTIPLIER))
+    plt.gca().yaxis.set_minor_formatter(NullFormatter())
+    plt.grid(True)
+    plt.minorticks_on()
+    plt.gca().set_yticks([REST_VOLTAGE, SPIKE_VOLTAGE])
+    plt.gca().set_xticks([])
+for i in range(1, len(CURRENTS)+1):
+    neuron = LIF(CAPACITANCE, RESISTANCE, SPIKE_VOLTAGE, REST_VOLTAGE)
+    current = CURRENTS[i-1]
+    plt.subplot(810+i+4)
+    plt.title('Current: {}'.format(current))
+    for i in range(int(TIME_STEPS*2/3)):
+        neuron.time_step(current)
+    for i in range(TIME_STEPS-int(TIME_STEPS*2/3)):
+        neuron.time_step(0)
+    neuron.add_plot(plt, 'r')
+    x1, x2, y1, y2 = plt.axis()
+    plt.axis((x1, x2, 0, SPIKE_VOLTAGE*SPIKE_MULTIPLIER))
+    plt.gca().yaxis.set_minor_formatter(NullFormatter())
+    plt.grid(True)
+    plt.minorticks_on()
+    plt.gca().set_yticks([REST_VOLTAGE, SPIKE_VOLTAGE])
+    plt.gca().set_xticks([])
+plt.subplots_adjust(top=0.95, bottom=0.1, left=0.1, right=0.95, hspace=0.25, wspace=0.25)
+plt.show()
 
-currents.append(constant_current)
+# Problem 2
+plt.figure(2)
+TIME_STEPS = 100
+X_VALS, Y_VALS = ([], [])
+for current in range(1, 100000):
+    X_VALS.append(current)
+    neuron = LIF(CAPACITANCE, RESISTANCE, SPIKE_VOLTAGE, REST_VOLTAGE)
+    for j in range(TIME_STEPS):
+        neuron.time_step(current)
+    spike_frequency = neuron.wave.values.count(SPIKE_VOLTAGE*SPIKE_MULTIPLIER)
+    Y_VALS.append(spike_frequency/TIME_STEPS)
+plt.plot(X_VALS, Y_VALS)
+plt.title('LIF Neuron: Input Current vs. Firing Rate')
+plt.xlabel('Input Current')
+plt.ylabel('Firing Rate')
+plt.show()
 
-# cap, res, spike, rest
-neuron = LIF(10, 10, 50, 10)
-
-for i in range(100):
-    neuron.time_step(currents[0](i))
-    print neuron.voltage
-
-neuron.wave.display()
+# Problem 3
+plt.figure(3)
+IZHI_U = -65*0.2
+IZHI_PARAMS = (0.02, 0.2, -65, 2) # a,b,c,d
+CURRENTS = []
+for x in range(0, 20, 2):
+    CURRENTS.append(x)
+for i in range(0, len(CURRENTS)):
+    neuron = Izhikevich(IZHI_U, IZHI_PARAMS)
+    current = CURRENTS[i]
+    plt.subplot(6, 2, i+1)
+    plt.title('Current: {}'.format(current))
+    for i in range(TIME_STEPS-1):
+        neuron.time_step(current)
+    neuron.add_plot(plt)
+    plt.grid(True)
+    plt.gca().set_xticks([])
+    plt.gca().set_yticks([IZHI_PARAMS[2], IZHIKEVICH_SPIKE])
+    x1, x2, y1, y2 = plt.axis()
+    plt.axis((x1, x2, IZHI_PARAMS[2], IZHIKEVICH_SPIKE*SPIKE_MULTIPLIER))
+plt.subplots_adjust(top=0.95, bottom=0.1, left=0.1, right=0.95, hspace=0.25, wspace=0.25)
+plt.show()
+"""
+# Problem 4
+plt.figure(4)
+HH_G = {'Na': 5, 'K': 6, 'L': 7}
+HH_V = {'Na': 5, 'K': 6, 'L': 7}
+HH_OPTS = {'h': 10, 'm': 20, 'n': 30}
+HH_CAPACITANCE = 10
+CURRENTS = []
+for x in range(0, 100, 10):
+    CURRENTS.append(x)
+for i in range(0, len(CURRENTS)):
+    neuron = HodgkinHuxley(HH_CAPACITANCE, HH_OPTS, HH_G, HH_V)
+    current = CURRENTS[i]
+    plt.subplot(5, 2, i+1)
+    plt.title('Current: {}'.format(current))
+    for i in range(TIME_STEPS-1):
+        neuron.time_step(current)
+    neuron.add_plot(plt)
+    plt.grid(True)
+plt.subplots_adjust(top=0.95, bottom=0.1, left=0.1, right=0.95, hspace=0.25, wspace=0.25)
+plt.show()
