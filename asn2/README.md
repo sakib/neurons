75134a998d7683dfba0a7175c095fe0ccc232275
Sakib Jalal <sakib.jalal@gmail.com>
2017-11-27 16:39:28 -0500

    asn2 complete

---
+++ asn2/README.md
@@ -0,0 +1,23 @@
+Sakib Jalal
+Brain-Inspired Computing
+Assignment 2
+
+`hebb_snn.py` contains my implementation
+of Hebbian Learning with firing-rate encoding.
+
+`stdp_snn.py` contains my implementation
+of Spike-Timing Dependent Plasticity learning
+with temporal time-to-first-spike encoding.
+
+`tempo.py` contains my implementation of the
+Tempotron which uses temporal time-to-first-spike
+encoding to classify images of 5x5 grids of lines.
+
+`lines/` contains my input images of 5x5 grids
+of lines which are fed into the tempotron.
+
+`graphs/` contains my output graphs from the
+execution of the tempotron on the input data.
+
+`neurons.py` contains my implementation of
+the LIF (Leaky-Integrate-and-Fire) neuron.
--- asn2/tempo.py
+++ asn2/tempo.py
@@ -1,14 +1,16 @@
-""" Spiking Neural Network implementation """
-import sys
-import math
-import numpy as np
-from random import uniform
+""" Tempotron Implementation """
+import sys, math, time, random
 from collections import defaultdict
 from neurons import LIF as Neuron
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 SMOL = 1. #2.5
 VALIDS = ['*']
+
+
+def round(flt, n_decimals=1):
+    return math.ceil(flt*10**n_decimals)/10**n_decimals
+
 
 def image_to_windows(image, n_neurons):
     with open('lines/{}'.format(image), 'r') as img:
@@ -35,12 +37,12 @@ class Tempotron(object):
         s.lambduh = .25     # learning rate
         s.on_ttfs = 4       # ms when 'on' neurons should spike
         s.t_threshold = 25  # time to wait for output to spike
-        s.V_rest, s.V_th = 0, n_inputs/2.
+        s.V_rest, s.V_th = 0, n_inputs
         s.neurons = [Neuron(i_neuron=i) for i in range(n_inputs)]
 
-        s.synapses = [uniform(-1, 1) for neuron in s.neurons]
+        s.synapses = [random.uniform(-1, 1) for neuron in s.neurons]
         while len(filter(lambda w: w < 0, s.synapses)) >= math.sqrt(n_inputs):
-            s.synapses = [uniform(-1, 1) for neuron in s.neurons] # limit inhibs
+            s.synapses = [random.uniform(-1, 1) for neuron in s.neurons] # limit inhibs
 
 
     def tau(self, sign='none'): # > 1
@@ -62,10 +64,8 @@ class Tempotron(object):
         classified, t_max, v_max = s.classify(image, switch_fn)
         weight_deltas = [s.weight_delta(neuron, t_max) for neuron in s.neurons]
         if not classified and trueImage: # increase all weights
-            #print('increasing weights')
             s.synapses = [w+dw for w,dw in zip(s.synapses, weight_deltas)]
         elif classified and not trueImage: # decrease all weights
-            #print('decreasing weights')
             s.synapses = [w-dw for w,dw in zip(s.synapses, weight_deltas)]
 
 
@@ -83,20 +83,18 @@ class Tempotron(object):
                 if switches[idx]: # make neuron spike
                     current = s.calibrate_current(s.on_ttfs)
                     s.neurons[idx].time_step(current, time)
-                    #if s.neurons[idx].just_spiked(time): print('{} SPIKE!'.format(idx))
             # calculate output voltage V(t) and track time of max voltage
             c_voltage = s.voltage(time)
             x_axis.append(time)
             y_axis.append(min(c_voltage, s.V_th))
-            #print(c_voltage)
             if c_voltage > v_max:
                 t_max = time
                 v_max = c_voltage
             if c_voltage >= s.V_th: # >
-                #plot.add_plot(x_axis, y_axis)
+                if plot: plot.plot(x_axis, y_axis)
                 return True, t_max, v_max
 
-        #plot.add_plot(x_axis, y_axis)
+        if plot: plot.plot(x_axis, y_axis)
         return False, t_max, v_max
 
 
@@ -108,7 +106,6 @@ class Tempotron(object):
             if neuron.has_spiked() and not neuron.just_spiked(time):
                 psps = s.psp_kernels(neuron, time)
                 voltage += weight * sum(psps) * 1./max(psps)
-        #print('t={} v={}'.format(time, voltage))
         return voltage
 
 
@@ -145,36 +142,61 @@ class Tempotron(object):
 
 if __name__ == '__main__':
 
+    # setup problem
     tempotron = Tempotron(9)
     problem = 'Line Detection'
     images = defaultdict()
     valid_angles = [0, 180]
     n_training_iters = 5
+    GRAPH = True #False
 
+    # methods of feeding windows from inputs to neurons
     switch_fns = defaultdict() # fns to convert window to bool
     switch_fns['center'] = lambda w: w[len(w)/2][len(w)/2] in VALIDS
     switch_fns['line'] = lambda w: all([c in VALIDS for c in w[len(w)/2]])
 
     for name, switch_fn in switch_fns.items():
+        # basic info
         print('\n{}\n{}'.format(problem, '-'*len(problem)))
-        print('detection method: {}\n'.format(name))
+        print('detection method: {}'.format(name))
+        print('graphing: {}\n'.format('on' if GRAPH else 'off'))
         for angle in range(0, 360, 20):
             images[angle] = 'angle_{}'.format(angle)
 
+        # train tempotron
         print('Training...')
+        start_time = time.time()
         for i in range(n_training_iters):
-            for angle in range(0, 360, 20):
+            for angle in range(0, 180, 20):
                 tempotron.train(images[angle], switch_fn, angle in valid_angles)
+        print('Avg time to train on all inputs: {}ms\n'.format(
+            round(1000*(time.time() - start_time)/n_training_iters, n_decimals=1)))
 
+        # output trained weights
         print('Weights...')
         tempotron.print_synapses()
+        print('')
 
-        # set valid threshold
-        classified, t_max, v_max = tempotron.classify(images[0], switch_fn, None)
+        # set valid truth time-to-first-spike threshold
+        truth, t_max, v_max = tempotron.classify(images[0], switch_fn)
         tempotron.V_th = v_max
 
+        # classify input images
         print('Classifying...'.format(v_max))
-        for angle in range(0, 360, 20): # yield ttfs
-            classified, t_max, v_max = tempotron.classify(images[angle], switch_fn, None)#, plt)
-            print('Angle {}:\t{}\t{}'.format(angle, classified, v_max))
+        max_voltages = []
+        start_time = time.time()
+        for angle in range(0, 360, 20):
+            if not GRAPH: truth, t_max, v_max = tempotron.classify(images[angle], switch_fn)
+            else: truth, t_max, v_max = tempotron.classify(images[angle], switch_fn, plt)
+            print('Angle {}:\t{}\t{}'.format(angle, truth, v_max))
+            max_voltages.append(v_max)
+            if GRAPH:
+                plt.savefig('graphs/{}_{}.png'.format(name, angle))
+                plt.clf()
+        if GRAPH:
+            plt.plot([angle for angle in range(0, 360, 20)], max_voltages)
+            plt.savefig('graphs/{}.png'.format(name))
+            plt.clf()
 
+        print('\nTotal time to classify all inputs: {}ms\n'.format(
+            round(1000*(time.time() - start_time)/(360/20), n_decimals=1)))
