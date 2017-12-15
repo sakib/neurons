bd4b1104c076031bb7269b9752003d1d74de828b
Sakib Jalal <sakib.jalal@gmail.com>
2017-12-15 18:23:03 -0500

    eeee

--- heatmaps/tempo_cnn.py
+++ heatmaps/tempo_cnn.py
@@ -6,71 +6,38 @@ from keras.layers import Convolution2D, MaxPooling2D  # Keras CNN Layers
 from keras.utils import np_utils
 from matplotlib import pyplot as plt
 from keras.datasets import mnist
+from dataset import MNIST
 from tempo_layer import DigitTempotronLayer
 
 
-np.random.seed(7)  # for reproducibility
-
-# load data, output matrices from tempotron
-# With 50 training images and 50 testing images
-# x_train should be 50*10 matrix, x_test should be 50*10 matrix
-# y_train should be 50*1 vector, y_test should be 50*1 vector
-dtl = DigitTempotronLayer()
-x_train = dtl.get_layer_output()[0]
-x_test = x_train
-y_train = dtl.get_layer_output()[1]
-y_test = y_train
-#(x_train, y_train), (x_test, y_test) = mnist.load_data()
-#x_train = x_train.reshape(x_train.shape[0], 784)
-#x_test = x_test.reshape(x_test.shape[0], 784)
-
-# Preprocess Input Matrices
-X_train = x_train.astype('float32')
-X_test = x_test.astype('float32')
-
-# normalize voltage inputs
-max_voltage = 90
-X_train = X_train / max_voltage
-X_test = X_test / max_voltage
-
-# Y_train should be 50*10 one hot matrix (encoded outputs)
-# Y_test should be 50*10 one hot matrix (encoded outputs)
-Y_train = np_utils.to_categorical(y_train)
-Y_test = np_utils.to_categorical(y_test)
-num_classes = Y_test.shape[1]
-"""
 # With 50 training images and 50 testing images
 # x_train should be 50*10*10 matrix, x_test should be 50*10*10 matrix
 # y_train should be 50*1 vector, y_test should be 50*1 vector
 dtl = DigitTempotronLayer()
 dataset = MNIST(n_components=100, reshape=False)
 np.random.seed(7)  # for reproducibility
+max_voltage = 64
 
 # Training data
-train_samples = []
-for digit in range(10): # 0->9
-    for ten_by_ten_matrix in dataset.sample(5, digit, digit): # 5 x 'digit'
-        train_samples.append(ten_by_ten_matrix)
-train_samples = np.asarray(train_samples).reshape(50, 100)
-X_train = train_samples.astype('float32')
-y_train = dtl.get_layer_output()[1]
+x_train, y_train = dtl.get_layer_output()
+X_train = x_train.astype('float32') / max_voltage # normalize
 Y_train = np_utils.to_categorical(y_train) # 50*10 one hot matrix (encoded outputs)
 
 # Testing data
-i = 0
-test_samples = []
-y_test = np.zeros((50, 1))
-for digit in range(10): # 0->9
-    for ten_by_ten_matrix in dataset.new_sample(5, digit):
-        test_samples.append(ten_by_ten_matrix)
-        y_test[i] = digit
-        i += 1
-test_samples = np.asarray(test_samples).reshape(50, 100)
-X_test = test_samples.astype('float32')
-Y_test = np_utils.to_categorical(y_test) # 50*10 one hot matrix (encoded outputs)
-num_classes = Y_test.shape[1]
-
+y_test = y_train
+x_test = x_train
 """
+new_x = []
+for digit in range(10): # 0->9
+    for vector in dataset.new_sample(1, digit):
+        #print('cock {}'.format(digit))
+        voltages = dtl.classify(vector) # output of tempotron layer, list len 10
+        new_x.append(voltages)
+        y_test = np.append(y_test, [digit])
+x_test = np.append(x_test, np.asarray(new_x)).reshape(len(x_test)+len(new_x), 10)
+"""
+X_test = x_test.astype('float32') / max_voltage
+Y_test = np_utils.to_categorical(y_test)
 
 
 def keras_model(n_hidden_layers, n_neurons):
@@ -92,11 +59,12 @@ def keras_model(n_hidden_layers, n_neurons):
     return model
 
 a = np.zeros((5, 10))
+scale = 10
 
-#for batch_size in [5, 10, 25, 50]:
 for batch_size in [5, 50]:
     for n_hidden_layers in range(1, 6): # 1->5
-        for n_neurons in range(100, 1100, 100): # 100->1000
+        #for n_neurons in range(100, 1100, 100): # 100->1000
+        for n_neurons in range(scale, scale**2+scale, scale): # 10->100
             # build the model
             model = keras_model(n_hidden_layers, n_neurons)
             # training the model and saving metrics in history
@@ -104,15 +72,16 @@ for batch_size in [5, 50]:
                       batch_size=batch_size, epochs=50, verbose=0,
                       validation_data=(X_test, Y_test))
 
-            a[n_hidden_layers-1][int(n_neurons/100)-1] = model.evaluate(X_test, Y_test, verbose=0)[1]
+            a[n_hidden_layers-1][int(n_neurons/scale)-1] = model.evaluate(X_test, Y_test, verbose=0)[1]
             print('bs: {}\th: {}\tn: {}\tacc: {}'.format(batch_size, n_hidden_layers,
-                n_neurons, a[n_hidden_layers-1][int(n_neurons/100)-1]))
+                n_neurons, a[n_hidden_layers-1][int(n_neurons/scale)-1]))
 
     plt.imshow(a, cmap=mpl.cm.get_cmap('Reds'), extent=[-0.5, 9.5, 0.5, 5.5])
+    plt.clim(vmin=0., vmax=1.)
     plt.title('Accuracy of Tempotron CNN, batch {}'.format(batch_size))
     plt.ylabel('n_hidden_layers')
     plt.yticks(np.arange(1, 6, 1))
     plt.xlabel('n_neurons')
-    plt.xticks(np.arange(10), (100*(x+1) for x in range(10)))
+    plt.xticks(np.arange(10), (scale*(x+1) for x in range(10)))
     plt.colorbar()
     plt.show()
